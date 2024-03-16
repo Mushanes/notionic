611f19b50e095bacd870673a856fc6d0f97e944a
Mushanes <61040916+Mushanes@users.noreply.github.com>
2024-03-16 23:11:41 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'i@museans.me',
   link: 'https://museans.top',
   newsletter: 'Notionic Weekly',
-  description: 'What's past is prologue',
+  description: 'What\'s past is prologue',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
