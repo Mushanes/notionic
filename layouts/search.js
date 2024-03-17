63cff886dcb7a932b9411e46d6a3bfc0b01c4451
Mushanes <61040916+Mushanes@users.noreply.github.com>
2024-03-17 14:53:37 +0800

    Update search.js

--- layouts/search.js
+++ layouts/search.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import BlogPost from '@/components/BlogPost'
 import Container from '@/components/Container'
 import Tags from '@/components/Common/Tags'
